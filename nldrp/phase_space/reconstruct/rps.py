a84cdbe055298c6c53089d321315775e28eee50b
thymios <etzinis@gmail.com>
2018-02-25 16:41:43 +0200

    Add signature in RPS

--- nldrp/phase_space/reconstruct/rps.py
+++ nldrp/phase_space/reconstruct/rps.py
@@ -1,3 +1,10 @@
+"""! 
+\brief Reconstruct the Phase Space of a Signal
+
+@author Efthymios Tzinis {etzinis@gmail.com}
+@copyright National Technical University of Athens
+"""
+
 import numpy as np
 
 def rps(signal, tau, ed):
