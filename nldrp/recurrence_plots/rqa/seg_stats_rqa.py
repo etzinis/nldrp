c08c6c42463290a960cfa52f65e5ccac54c2ef7a
Efthymios Tzinis <etzinis@gmail.com>
2018-03-05 19:45:04 +0200

    Seg stats rqa initial doics

---
+++ nldrp/recurrence_plots/rqa/seg_stats_rqa.py
@@ -0,0 +1,9 @@
+"""!
+\brief Recurrence Quantification Analysis (RQA) feature extraction
+for deltas and global statistics extraction for a segment. This
+breaks to frames of the configured length (infered from duration in
+seconds and the fs)
+
+@author Efthymios Tzinis {etzinis@gmail.com}
+@copyright National Technical University of Athens
+"""
