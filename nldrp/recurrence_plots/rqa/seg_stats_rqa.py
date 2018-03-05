55e9745143a9fc667056d0c0aa697d9c9582ea6c
Efthymios Tzinis <etzinis@gmail.com>
2018-03-05 22:33:33 +0200

    Reinitializing rqa frame level in order to remove caching information

--- nldrp/recurrence_plots/rqa/seg_stats_rqa.py
+++ nldrp/recurrence_plots/rqa/seg_stats_rqa.py
@@ -19,7 +19,8 @@ nldrp_dir = os.path.join(
 sys.path.insert(0, nldrp_dir)
 
 import nldrp.feature_extraction.frame_breaker as frame_breaker
-
+import nldrp.feature_extraction.stat_funcs as stats_funcs
+import nldrp.feature_extraction.deltas as deltas
 
 class SegmentRQAStatistics(object):
 
@@ -41,17 +42,35 @@ class SegmentRQAStatistics(object):
         self.frame_stride=frame_stride
         self.fs=fs
 
+        self.phase_space_method = phase_space_method
+        self.time_lag = time_lag
+        self.embedding_dimension = embedding_dimension
+        self.norm = norm
+        self.thresh_method = thresh_method
+        self.thresh = thresh
+        self.l_min = l_min
+        self.v_min = v_min
+        self.w_min = w_min
+
+
+    def frame_rqa_extraction(self,
+                             frame):
+
         # configure the frame extractor as supposed to
-        self.rqa_extractor = rqa.RQA(
-                phase_space_method=phase_space_method,
-                time_lag=time_lag,
-                embedding_dimension=embedding_dimension,
-                norm=norm,
-                thresh_method=thresh_method,
-                thresh=thresh,
-                l_min=l_min,
-                v_min=v_min,
-                w_min=w_min)
+        frame_rqa_extractor = rqa.RQA(
+                phase_space_method=self.phase_space_method,
+                time_lag=self.time_lag,
+                embedding_dimension=self.embedding_dimension,
+                norm=self.norm,
+                thresh_method=self.thresh_method,
+                thresh=self.thresh,
+                l_min=self.l_min,
+                v_min=self.v_min,
+                w_min=self.w_min)
+
+        # extract the rqa measures for this frame
+        return frame_rqa_extractor.RQA_extraction(frame)
+
 
     def extract(self,
                 signal):
@@ -66,13 +85,22 @@ class SegmentRQAStatistics(object):
                                    frame_stride=self.frame_stride)
         frames_st_inds, frame_size, frame_step = frame_info
 
-        frame_rqa = np.array(
-            [self.rqa_extractor.RQA_extraction(
-             signal[x:x + frame_size])
-             for x in frames_st_inds]
+        frames_rqa = np.array(
+            [self.frame_rqa_extraction(signal[st:st + frame_size])
+             for st in frames_st_inds]
         )
 
-        print frame_rqa.shape
+        frames_rqa_d = deltas.compute(frames_rqa)
+
+        print frames_rqa[5]
+        print frames_rqa[4]
+
+        rqa_stats = stats_funcs.compute(frames_rqa)
+        rqa_d_stats = stats_funcs.compute(frames_rqa_d)
+
+        whole_stats = np.concatenate([rqa_stats, rqa_d_stats], axis=0)
+
+        return whole_stats
 
 
 if __name__ == '__main__':
@@ -80,8 +108,10 @@ if __name__ == '__main__':
     frame_duration = 0.02
     frame_stride = 0.01
     fs=44100
-    s_len=int(3.0*fs)
+    s_len=int(0.3*fs)
     signal = np.random.normal(0., 1., s_len)
+
+    # signal = np.cos((2. * np.pi * 30 / fs) * np.arange(s_len))
 
     seg_extr = SegmentRQAStatistics(
                  phase_space_method='ad_hoc',
@@ -97,4 +127,4 @@ if __name__ == '__main__':
                  frame_stride=frame_stride,
                  fs=fs)
 
-    seg_extr.extract(signal)
+    stats = seg_extr.extract(signal)
