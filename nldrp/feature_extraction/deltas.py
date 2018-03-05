5d51389e8112325980561f0d72a4f409942cb443
Efthymios Tzinis <etzinis@gmail.com>
2018-03-05 18:00:19 +0200

    Update example of usage vecotorize implementation

--- nldrp/feature_extraction/deltas.py
+++ nldrp/feature_extraction/deltas.py
@@ -22,5 +22,17 @@ def compute(features_block):
     represents the raw features over time that we want to extract
     stats from. (n_samples-1, n_features) """
 
-    features_block[]
+    return (features_block[1:]-features_block[0:-1])
 
+
+def sanity_test():
+    import numpy as np
+
+    a = np.array([[1,2,3,4],[5,6,7,8],[9,10,11,12]], dtype=np.float32)
+    da = compute(a)
+
+    print a
+    print da
+
+if __name__ == "__main__":
+    sanity_test()
