e7dcdaa3daa2a1fa1d7c4ba8d9f23fd559836d5c
Efthymios Tzinis <etzinis@gmail.com>
2018-03-05 17:49:35 +0200

    dEltas documentation and usage

---
+++ nldrp/feature_extraction/deltas.py
@@ -0,0 +1,26 @@
+"""!
+\brief Deltas module for computing the differences from a
+given 2d numpy matrix over all rows.
+
+@author Efthymios Tzinis {etzinis@gmail.com}
+@copyright National Technical University of Athens
+"""
+
+def compute(features_block):
+    """!
+    \brief Computes the deltas in between the rows of the given
+    features_block
+
+    \param features_block (\a numpy 2D array) 2D numpy block which
+    represents the raw features over time that we want to extract
+    stats from. (n_samples, n_features)
+
+    \warning Caller function is responsible for checking the validity
+    of both parameters.
+
+    \param features_block (\a numpy 2D array) 2D numpy block which
+    represents the raw features over time that we want to extract
+    stats from. (n_samples-1, n_features) """
+
+    features_block[]
+
