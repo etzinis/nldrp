56d620774d9f12d11bf78f94d5eca439664d6df1
Efthymios Tzinis <etzinis@gmail.com>
2018-03-06 02:04:24 +0200

    configure in file SAVEE path

--- nldrp/config.py
+++ nldrp/config.py
@@ -2,3 +2,4 @@ import os
 
 BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PYUNICORN_PATH = '/home/thymios/Desktop/Research/pyunicorn/'
+SAVEE_PATH = '/home/thymios/Desktop/Research/datasets/SAVEE/'
