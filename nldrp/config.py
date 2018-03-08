7229cdd82d67d113bb81cbab31c7aa74589f3950
Efthymios Tzinis <etzinis@gmail.com>
2018-03-09 01:06:25 +0200

    Adding opensmile config path to nldrp config

--- nldrp/config.py
+++ nldrp/config.py
@@ -4,3 +4,5 @@ BASE_PATH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PYUNICORN_PATH = '/home/thymios/Desktop/Research/pyunicorn/'
 SAVEE_PATH = '/home/thymios/Desktop/Research/datasets/SAVEE/'
 EXTRACTED_FEATURES_PATH = '/home/thymios/Desktop/Research/data/'
+OPENSMILE_CONFIG_PATH = '/home/thymios/Desktop/Research/opensmile-2.3' \
+                        '.0/config/emobase2010.conf'
