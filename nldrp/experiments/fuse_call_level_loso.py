f880e5642da05e981551ad78ba5468acb1d589b8
Efthymios Tzinis <etzinis@gmail.com>
2018-03-09 04:40:44 +0200

    Normalizing per speaker

--- nldrp/experiments/fuse_call_level_loso.py
+++ nldrp/experiments/fuse_call_level_loso.py
@@ -21,6 +21,8 @@ from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, Gradien
 from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
 
+from sklearn.externals import joblib
+
 import argparse
 import numpy as np
 import pprint
@@ -83,17 +85,17 @@ def compute_metrics(Y_predicted, Y_true):
 
 def configure_models():
     models = []
-    models.append(('LR', LogisticRegression()))
+    # models.append(('LR', LogisticRegression()))
     models.append(('LDA', LinearDiscriminantAnalysis()))
     models.append(('KNN', KNeighborsClassifier()))
-    models.append(('CART', DecisionTreeClassifier()))
-    models.append(('NB', GaussianNB()))
+    # models.append(('CART', DecisionTreeClassifier()))
+    # models.append(('NB', GaussianNB()))
     models.append(('SVM', SVC()))
     models.append(('RF', RandomForestClassifier()))
-    models.append(('ADAb', AdaBoostClassifier()))
-    models.append(('GRADb', GradientBoostingClassifier()))
-    models.append(('QDA', QuadraticDiscriminantAnalysis()))
-    models.append(('LinR', LogisticRegression()))
+    # models.append(('ADAb', AdaBoostClassifier()))
+    # models.append(('GRADb', GradientBoostingClassifier()))
+    # models.append(('QDA', QuadraticDiscriminantAnalysis()))
+    # models.append(('LinR', LogisticRegression()))
     return dict(models)
 
 
@@ -104,23 +106,127 @@ def evaluate_fold(model,
     from sklearn.preprocessing import StandardScaler
     from sklearn.decomposition import PCA
     from sklearn.manifold import LocallyLinearEmbedding
-    # pca = PCA(n_components=int(X_tr.shape[1] / 10)).fit(X_tr)
-    n_components = int(X_tr.shape[1] / 10)
-    pca = LocallyLinearEmbedding(n_components=n_components,
-                                 n_neighbors=(n_components + 1),
-                                 method='modified').fit(X_tr)
-    X_tr = pca.transform(X_tr)
+
+    n_components = int(X_tr.shape[1] / 2)
+    # n_components = 1500
+    pca = PCA(n_components=n_components).fit(X_tr)
+    # # pca = LocallyLinearEmbedding(n_components=n_components,
+    # #                              n_neighbors=(n_components + 1),
+    # #                              method='modified').fit(X_tr)
+    # X_tr = pca.transform(X_tr)
     scaler = StandardScaler().fit(X_tr)
-    X_tr_scaled = scaler.transform(X_tr)
-    model.fit(X_tr_scaled, Y_tr)
-    X_te = pca.transform(X_te)
-    X_te_scaled = scaler.transform(X_te)
-    Y_pred = model.predict(X_te_scaled)
+    X_tr = scaler.transform(X_tr)
+    model.fit(X_tr, Y_tr)
+    # X_te = pca.transform(X_te)
+    scaler_te = StandardScaler().fit(X_te)
+    X_te = scaler_te.transform(X_te)
+    Y_pred = model.predict(X_te)
     model_metrics = compute_metrics(Y_pred, Y_te)
     return model_metrics
 
 
+def evaluate_loso(features_dic):
+    all_models = configure_models()
+    result_dic = {}
+
+    for model_name, model in all_models.items():
+        result_dic[model_name] = {}
+        for te_speaker, X_te, Y_te, X_tr, Y_tr in generate_speaker_folds(
+                features_dic):
+
+            fold_info = evaluate_fold(model, X_te, Y_te, X_tr, Y_tr)
+
+            if result_dic[model_name]:
+                for k, v in fold_info.items():
+                    result_dic[model_name][k].append(v)
+            else:
+                for k, v in fold_info.items():
+                    result_dic[model_name][k] = [v]
+
+        print model_name
+        for k, v in result_dic[model_name].items():
+            result_dic[model_name][k] = '{} +- {}'.format(
+                np.mean(v), np.std(v))
+            print k, result_dic[model_name][k]
+
+    # pprint.pprint(result_dic)
+
+
+def convert_2_numpy_per_utterance(dataset_dic):
+    converted_dic = {}
+    for spkr in dataset_dic:
+        x_list = []
+        y_list = []
+        converted_dic[spkr] = {}
+        for id, el_dic in dataset_dic[spkr].items():
+            label = el_dic['y']
+            feat_vec = el_dic['x']
+            x_list.append(feat_vec)
+            y_list.append(label)
+
+        this_utt_array = np.array(x_list)
+        converted_dic[spkr]['x']=this_utt_array
+        converted_dic[spkr]['y']=y_list
+
+    return converted_dic
+
+
 def fusion_loso(list_of_paths):
-    pass
+
+    try:
+        feat_p = list_of_paths.pop(0)
+        final_data_dic = joblib.load(feat_p)
+    except Exception as e:
+        print "At least one file path is required"
+        raise e
+
+    while list_of_paths:
+        feat_p = list_of_paths.pop(0)
+        temp_dic = joblib.load(feat_p)
+        try:
+            for spkr in temp_dic:
+                for id, el_dic in temp_dic[spkr].items():
+                    assert el_dic['y'] == final_data_dic[spkr][id]['y']
+                    prev_vec = final_data_dic[spkr][id]['x']
+                    this_vec = el_dic['x']
+                    new_vec = np.concatenate([prev_vec, this_vec],
+                                             axis=0)
+                    final_data_dic[spkr][id]['x'] = new_vec
+        except Exception as e:
+            print "Failed to update the Fused dictionary"
+            raise e
+
+    fused_converted_dic = convert_2_numpy_per_utterance(final_data_dic)
+    evaluate_loso(fused_converted_dic)
 
 
+def get_args():
+    """! Command line parser for Utterance level classification Leave
+    one speaker out schema pipeline"""
+    parser = argparse.ArgumentParser(
+        description='Utterance level classification Leave one '
+                    'speaker out schema pipeline' )
+    parser.add_argument('-i', '--input_features_paths', nargs='+',
+                        help='File paths of the features you want to '
+                             'concatenate and the classify')
+
+    # parser.add_argument("--dataset", type=str,
+    #                     help="""The name of the dataset""",
+    #                     required=True,
+    #                     choices=['SAVEE'])
+    # parser.add_argument("-i", "--save_dir", type=str,
+    #     help="""Where the corresponding binary file full of
+    #     data that will contain the dictionary for each speaker is
+    #     stored.
+    #     Another subdic for all the sentences with their ids
+    #     and a 1d numpy matrix for each one of them.
+    #     """,
+    #     default=nldrp.config.EXTRACTED_FEATURES_PATH )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    """!brief Example of usage"""
+    args = get_args()
+    fusion_loso(args.input_features_paths)
