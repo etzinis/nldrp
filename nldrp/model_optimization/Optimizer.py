b6b674ccb1f94f0c55c8da765d3fdceeab5379ae
Efthymios Tzinis <etzinis@gmail.com>
2018-03-11 17:43:34 +0200

    Optimizer constructor with checking for errors in definitions

--- nldrp/model_optimization/Optimizer.py
+++ nldrp/model_optimization/Optimizer.py
@@ -6,6 +6,16 @@ to the specification for a metric.
 @author Efthymios Tzinis {etzinis@gmail.com}
 @copyright National Technical University of Athens
 """
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+
 
 
 def compute_metrics(Y_predicted, Y_true):
@@ -29,8 +39,47 @@ def compute_metrics(Y_predicted, Y_true):
 class ModelOptimizer(object):
     def __init__(self,
                  model_name,
-                 params_grid_space,
-                 metric_to_optimize):
+                 folds_generator,
+                 params_grid,
+                 metrics_to_optimize):
+
+        valid_models = ['SVM', 'LR']
+        if model_name in valid_models:
+            self.model_name = model_name
+        else:
+            raise NotImplementedError(('Model: <{}> is not yet '
+                                       'supported. Please try one of '
+                                       'the following: {}'.format(
+                                        model_name, valid_models)))
+
+        self.param_grid = params_grid
+        self.folds_gen = folds_generator
+
+        valid_metrics = ['uw_f1', 'uw_acc', 'w_acc']
+        assert type(metrics_to_optimize) == type([]), 'Metrics to ' \
+                                                      'optimize ' \
+                                                      'should be ' \
+                                                      'defined in a ' \
+                                                      'list'
+        for m in metrics_to_optimize:
+            if m not in valid_metrics:
+                raise NotImplementedError(('Metric to optimize: <{}> '
+                                           'is not yet '
+                                           'supported. Please try '
+                                           'one of '
+                                           'the following: {}'.format(
+                                            m, valid_metrics)))
+        self.opt_metrics = metrics_to_optimize
+
+
+    @staticmethod
+    def configure_model(model_name):
+        if model_name == 'SVM':
+            return
+
+
+
+
 
 
 
