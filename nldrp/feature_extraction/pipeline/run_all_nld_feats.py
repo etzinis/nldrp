100892190d23e355a386373fdb44b68b5396840e
Efthymios Tzinis <etzinis@gmail.com>
2018-03-14 11:56:46 +0200

    Make runs for nld feature extraction configurable

--- nldrp/feature_extraction/pipeline/run_all_nld_feats.py
+++ nldrp/feature_extraction/pipeline/run_all_nld_feats.py
@@ -18,45 +18,72 @@ valid_thresh_methods = ["threshold",
                         "threshold_std",
                         "recurrence_rate"]
 
-valid_norms = ["manhattan", "euclidean", "supremum"]
+valid_norms = ["manhattan", "euclidean"]
 
-threshold_ratios = [0.05, 0.1, 0.15]
+threshold_ratios = [0.1, 0.15, 0.2]
 
-frame_durations = [0.02, 0.03, 0.05]
+frame_durations = [0.02, 0.03]
 
-taus = [1, 7]
+taus = [7]
 
-cnt = 1
 search_space = (len(valid_thresh_methods) *
                 len(threshold_ratios) *
                 len(frame_durations) *
                 len(valid_norms))
 
-for thres_method in valid_thresh_methods:
-    for norm in valid_norms:
-        for thres in threshold_ratios:
-            for fd in frame_durations:
-                print "Extracting {}/{}...".format(cnt, search_space)
-                cnt += 1
-                configa={
-                    'dataset':'SAVEE',
-                    'cache_dir': '/tmp/',
-                    'save_dir':nldrp.config.EXTRACTED_FEATURES_PATH,
-                    'phase_space_method':'ad_hoc',
-                    'time_lag':1,
-                    'embedding_dimension':3,
-                    'norm':norm,
-                    'thresh_method':thres_method,
-                    'thresh':thres,
-                    'l_min':2,
-                    'v_min':2,
-                    'w_min':1,
-                    'frame_duration':fd,
-                    'frame_stride':fd / 2.0
-                }
- 
-                try:
-                    feat_extract.run(configa)
-                except Exception as e:
-                    print(e)
-                    exit()
+def main(args):
+    cnt = 1
+    for thres_method in valid_thresh_methods:
+        for norm in valid_norms:
+            for thres in threshold_ratios:
+                for fd in frame_durations:
+                    print "Extracting {}/{}...".format(cnt, search_space)
+                    cnt += 1
+                    configa={
+                        'dataset':args.dataset,
+                        'cache_dir': '/tmp/',
+                        'save_dir':'/home/thymios/Desktop/Research'
+                                   '/all_'+args.dataset+'_features/rqa/',
+                        'phase_space_method':'ad_hoc',
+                        'time_lag':7,
+                        'embedding_dimension':3,
+                        'norm':norm,
+                        'thresh_method':thres_method,
+                        'thresh':thres,
+                        'l_min':2,
+                        'v_min':2,
+                        'w_min':1,
+                        'frame_duration':fd,
+                        'frame_stride':fd / 2.0
+                    }
+
+                    try:
+                        feat_extract.run(configa)
+                    except Exception as e:
+                        print(e)
+                        exit()
+
+
+def get_args():
+    """! Command line parser for Utterance level feature extraction
+    pipeline"""
+    parser = argparse.ArgumentParser(
+        description='Utterance level feature extraction pipeline' )
+    parser.add_argument("--cache_dir", type=str,
+        help="""Directory which would be available to store some 
+        binary files for quicker load of dataset""",
+        default='/tmp/')
+    parser.add_argument("--dataset", type=str,
+                        help="""The name of the dataset""",
+                        required=True,
+                        choices=['SAVEE',
+                                 'IEMOCAP',
+                                 'BERLIN'])
+    
+    args = parser.parse_args()
+    return args
+
+if __name__ == "__main__":
+    """!brief Example of usage"""
+    args = get_args()
+    main(args)
