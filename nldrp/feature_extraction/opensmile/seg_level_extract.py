c7759712a3e7c67d467d4413d0570dafce467995
Efthymios Tzinis <etzinis@gmail.com>
2018-03-16 15:36:44 +0200

    Segment level feature extraction initial copy from utterance and arguments fro the command line

---
+++ nldrp/feature_extraction/opensmile/seg_level_extract.py
@@ -0,0 +1,209 @@
+"""!
+\brief Opensmile Segment-level extraction by using a specified
+configuration file
+
+\warning you are assume to have opensmile already installed and
+exported the appropriate paths to your corresponding .bashrc | .zshrc
+etc.
+
+@author Efthymios Tzinis {etzinis@gmail.com}
+@copyright National Technical University of Athens
+"""
+
+from scipy.io import arff
+import argparse
+import os
+import sys
+import numpy as np
+from sklearn.externals import joblib
+from progress.bar import ChargingBar
+import subprocess
+
+nldrp_dir = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    '../../../')
+sys.path.insert(0, nldrp_dir)
+import nldrp.config
+import nldrp.io.dataloader as dl_savee
+import nldrp.io.dataloader_emodb as dl_berlin
+import nldrp.io.dataloader_iemo as dl_iemocap
+import nldrp.feature_extraction.frame_breaker as seg_breaker
+
+
+def load_data(dataset_name):
+    if dataset_name == 'SAVEE':
+        loader_obj = dl_savee.SaveeDataloader(
+                      savee_path=nldrp.config.SAVEE_PATH)
+        dataset_dic = loader_obj.data_dict
+    elif dataset_name == 'IEMOCAP':
+        loader_obj = dl_iemocap.IemocapDataLoader(
+            iemocap_path=nldrp.config.IEMOCAP_PATH)
+        dataset_dic = loader_obj.data_dict
+    elif dataset_name == 'BERLIN':
+        loader_obj = dl_berlin.EmodbDataLoader(
+            emodb_path=nldrp.config.BERLIN_PATH)
+        dataset_dic = loader_obj.data_dict
+    else:
+        raise NotImplementedError('Dataset: {} is not yet supported '
+                                  ''.format(dataset_name))
+
+    return dataset_dic
+
+
+def opensmile_extract(config_p,
+                      wavpath,
+                      temp_p):
+    extr_command = ['SMILExtract', '-noconsoleoutput',
+                    '-C', config_p,
+                    '-I', wavpath,
+                    '-O', temp_p]
+
+    subprocess.call(extr_command)
+
+    data = arff.loadarff(temp_p)
+    feature_vec = np.asarray(data[0].tolist(), dtype=np.float32)
+
+    feature_vec = feature_vec.reshape(-1)[0:-1]
+    subprocess.call(['rm', temp_p])
+    return feature_vec
+
+
+def extract_per_segment(config_p,
+                        wavpath,
+                        temp_p,
+                        segment_,
+                        segment_ol,
+                        fs,
+                        signal):
+
+    seg_st_indices = seg_breaker.get_frames_start_indices(signal,
+                                                          fs,
+                                                          segment_len,
+                                                          segment_ol)
+    return None
+
+
+def get_features_dic(dataset_dic,
+                     config_p,
+                     segment_len,
+                     segment_ol):
+    features_dic = {}
+    total = sum([len(v) for k, v in dataset_dic.items()])
+    bar = ChargingBar("Extracting Opensmile Features for {} "
+                      "utterances...".format(total), max=total)
+    for spkr in dataset_dic:
+        features_dic[spkr] = {}
+        for id, raw_dic in dataset_dic[spkr].items():
+            features_dic[spkr][id] = {}
+
+            fs = raw_dic['Fs']
+            signal = raw_dic['wav']
+            wavpath = raw_dic['wavpath']
+
+            segment_features_2D = extract_per_segment(
+                                  config_p,
+                                  wavpath,
+                                  '/tmp/opensmile_feats_tmp',
+                                  segment_len,
+                                  segment_ol,
+                                  fs,
+                                  signal)
+
+            exit()
+
+            features_dic[spkr][id]['x'] = segment_features_2D
+            features_dic[spkr][id]['y'] = raw_dic['emotion']
+
+            bar.next()
+    bar.finish()
+    return features_dic
+
+
+def safe_mkdirs(path):
+    """! Makes recursively all the directory in input path """
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except Exception as e:
+            raise IOError(
+                ("Failed to create recursive directories: "
+                " {}".format(path)
+                )
+            )
+
+
+def save_features_dic(opensm_config,
+                      features_dic,
+                      save_dir):
+    utterance_save_dir = os.path.join(save_dir, 'segment/')
+    safe_mkdirs(utterance_save_dir)
+    save_p = os.path.join(utterance_save_dir, opensm_config)
+    print "Saving Opensmile Features Dictionary in {}".format(save_p)
+    joblib.dump(features_dic, save_p, compress=3)
+    print "OK!"
+
+
+
+def run(dataset,
+        save_dir,
+        config_p,
+        segment_len,
+        segment_ol):
+
+        print "Parsing Dataset <{}>...".format(dataset)
+        dataset_dic = load_data(dataset)
+        print "OK!"
+
+        features_dic = get_features_dic(dataset_dic,
+                                        config_p,
+                                        segment_len,
+                                        segment_ol)
+
+        exit()
+        opensm_config = dataset+'_linear_emobase2010'
+        save_features_dic(opensm_config, features_dic, save_dir)
+
+def get_args():
+    """! Command line parser for Opensmile Segment level feature
+    extraction pipeline"""
+    parser = argparse.ArgumentParser(
+        description='Opensmile Utterance level feature extraction '
+                    'pipeline' )
+    parser.add_argument("--dataset", type=str,
+                        help="""The name of the dataset""",
+                        required=True,
+                        choices=['SAVEE',
+                                 'IEMOCAP',
+                                 'BERLIN'])
+    parser.add_argument("-o", "--save_dir", type=str,
+        help="""Where to store the corresponding binary file full of 
+        data that will contain the dictionary for each speaker. 
+        Another subdic for all the sentences with their ids  
+        and a 1d numpy matrix for each one of them.
+        """,
+        default=nldrp.config.EXTRACTED_FEATURES_PATH)
+    parser.add_argument("--config", type=str,
+                        help="""Opensmile configuration PAth""",
+                        required=False,
+                        default=nldrp.config.OPENSMILE_CONFIG_PATH)
+    parser.add_argument("--segment_dur", type=float,
+                        help="""The specified length of the segments""",
+                        required=False,
+                        default=1.0)
+    parser.add_argument("--segment_ol_r", type=float,
+                        help="""The specified overlap ratio between the 
+                        segments""",
+                        required=False,
+                        default=0.5)
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    """!brief Example of usage"""
+    args = get_args()
+    run(args.dataset,
+        args.save_dir,
+        args.config,
+        args.segment_len,
+        args.segment_ol_r)
