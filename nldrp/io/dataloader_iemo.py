7a24609fe3f568f20c8df94b2a8527fac62dd4b9
Nick Ellinas <ngreek54@gmail.com>
2018-03-18 01:12:20 +0200

    New iemocap dataloder bugfixed

--- nldrp/io/dataloader_iemo.py
+++ nldrp/io/dataloader_iemo.py
@@ -2,6 +2,7 @@ import argparse
 import os
 import re
 import sys
+from pprint import pprint
 
 nldpr_dir = os.path.join(
     os.path.dirname(os.path.realpath(__file__)),
@@ -19,8 +20,8 @@ class IemocapDataLoader(object):
         else:
             self.data_dir = os.path.join(BASE_PATH, 'dataset_iemo')
         self.sessions = ['Session1', 'Session2', 'Session3', 'Session4', 'Session5']
-        self.speaker_ids = ['Ses01F', 'Ses01M', 'Ses02F', 'Ses02M', 'Ses03F', 'Ses03M', 'Ses04F', 'Ses04M', 'Ses05F',
-                            'Ses05M']
+        self.speaker_ids = ['Ses01F', 'Ses01M', 'Ses02F', 'Ses02M', 'Ses03F', 'Ses03M',
+                            'Ses04F', 'Ses04M', 'Ses05F', 'Ses05M']
         self.emotion_dict = {"ang": "angry", "hap": "happy",
                              "sad": "sad", "neu": "neutral",
                              "fru": "frustrated", "exc": "excited",
@@ -32,51 +33,87 @@ class IemocapDataLoader(object):
         self.data_dict = self.make_data_dict()
 
 
+    def get_utterance_with_lab(self,
+                               annotations_dir,
+                               dialogue):
+        utt_labels = {}
+        an_p = os.path.join(annotations_dir,
+                            dialogue + '.txt')
+
+        with open(an_p) as fd:
+            an_lines = fd.readlines()
+
+        for a_line in an_lines:
+            if dialogue in a_line:
+                dur, utt, em, _ = a_line.split('\t')
+                utt_labels[utt] = em
+
+        return utt_labels
+
+
+    def get_info_for_utt(self,
+                         utt_id,
+                         true_emotion,
+                         wavs_dir):
+
+        useful = utt_id.split("_")
+        fake_ses = useful[0]
+        mf_number = useful[-1]
+        script_name = '_'.join(useful[1:-1])
+
+        speaker_gender = mf_number[0]
+        true_session = fake_ses[:-1]
+        true_speaker = true_session + speaker_gender
+
+        fake_identifier = fake_ses + '_' + script_name
+
+        wavpath = os.path.join(wavs_dir, fake_identifier,
+                               utt_id + '.wav')
+        audiofile = AudioFile(wavpath)
+        fs = audiofile.get_fs()
+        wav_dur = audiofile.get_duration_seconds()
+        wav = audiofile.read_audio_file(normalize=True, mix_channels=True)
+
+        info = {
+            'speaker': true_speaker,
+            'wavpath': wavpath,
+            'emotion': true_emotion,
+            'Fs': fs,
+            'wav': wav,
+            'wav_duration': wav_dur
+        }
+
+        return info
+
 
     def make_data_dict(self):
         data_dict = {spk: {} for spk in self.speaker_ids}
         for session in self.sessions:
-            wav_dir = os.path.join(self.data_dir, session,
-                                   'sentences', 'wav')
-            for subsession in sorted(os.listdir(wav_dir)):
-                annotation_file = os.path.join(self.data_dir, session, 'dialog', 'EmoEvaluation', subsession + '.txt')
-                with open(annotation_file) as f:
-                    annotations = [line for line in f if re.search(subsession, line)]
-                annotations_dict = {el.split('\t')[-3]: el.split('\t')[-2] for el in annotations}
-                for utt in sorted(os.listdir(os.path.join(wav_dir, subsession))):
-                    if not utt.endswith('.wav'):
-                        continue
-                    uttid = utt.replace('.wav', '')
-                    speaker = uttid.split('_', 1)[0]
-                    alnum = uttid.split('_', 1)[1]
-                    session = 'Session' + speaker[-2]
-                    # Emotion
-                    emotion = self.emotion_dict[annotations_dict[uttid]]
-                    if emotion == 'unclassified' or emotion == 'other' or emotion == 'invalid':
-                        continue
+            wavs_dir = os.path.join(self.data_dir, session,
+                                    'sentence', 'wav')
+            annotations_dir = os.path.join(self.data_dir,
+                                           session, 'dialog',
+                                           'EmoEvaluation')
 
-                    # keep only the utterances that we will use in
-                    # the final classification
-                    if emotion not in self.emotions_in_use:
-                        continue
-                    # Fs, wav, wav_duration and normalize wav to [-1,1]
-                    wavpath = os.path.abspath(
-                        os.path.join(self.data_dir, session,
-                                     'sentences', 'wav', subsession, uttid + '.wav'))
-                    audiofile = AudioFile(wavpath)
-                    fs = audiofile.get_fs()
-                    wav_dur = audiofile.get_duration_seconds()
-                    wav = audiofile.read_audio_file(normalize=True, mix_channels=True)
-                    # Dictionary construction
-                    info = {
-                        'speaker': speaker,
-                        'wavpath': wavpath,
-                        'emotion': emotion,
-                        'Fs': fs,
-                        'wav': wav,
-                        'wav_duration': wav_dur
-                    }
-                    data_dict[speaker][alnum] = info
+            dialogues = os.listdir(wavs_dir)
+
+            for dialogue in dialogues:
+                utt_with_labels = self.get_utterance_with_lab(
+                                  annotations_dir, dialogue)
+
+                for utt, fake_emotion in utt_with_labels.items():
+                    true_emotion = self.emotion_dict[fake_emotion]
+                    if true_emotion in self.emotions_in_use:
+                        try:
+                            info = self.get_info_for_utt(utt,
+                                                     true_emotion,
+                                                     wavs_dir)
+                            data_dict[info['speaker']][utt] = info
+
+                        except Exception as e:
+                            print e
+                            print utt + "Failed to load"
+
         return data_dict
 
 
@@ -94,8 +131,9 @@ def get_args():
 if __name__ == "__main__":
     """!brief Example of usage"""
     args = get_args()
-    savee_data_obj = IemocapDataLoader(iemocap_path=args.iemocap_path)
+    iemo_data_obj = IemocapDataLoader(iemocap_path=args.iemocap_path)
     from pprint import pprint
 
-    pprint(savee_data_obj.data_dict['Ses04M']['script01_1_F003'])
-    # pprint(savee_data_obj.data_dict['DC']['a01']['phone_details'][0])
+    dataset_dic = iemo_data_obj.data_dict
+    for spkr in dataset_dic:
+        print spkr, len(dataset_dic[spkr])
