113ab45741cf48956b10bb0a3192b625c7407f57
Efthymios Tzinis <etzinis@gmail.com>
2018-03-18 17:38:42 +0200

    Finshing saving fused dictionary to appropriate path

--- nldrp/feature_extraction/seg_feats_fuser.py
+++ nldrp/feature_extraction/seg_feats_fuser.py
@@ -8,7 +8,7 @@
 import argparse
 import numpy as np
 from sklearn.externals import joblib
-import copy
+import os
 
 
 def fuse_all_configurations(list_of_paths):
@@ -27,11 +27,18 @@ def fuse_all_configurations(list_of_paths):
         try:
             for spkr in temp_dic:
                 for id, el_dic in temp_dic[spkr].items():
-                    assert el_dic['y'] == final_data_dic[spkr][id]['y']
-                    prev_vec = list(final_data_dic[spkr][id]['x'])
-                    this_vec = list(el_dic['x'])
-                    new_vec = np.array(prev_vec+this_vec,
-                                       dtype=np.float32)
+                    assert el_dic['y'] == final_data_dic[spkr][id][
+                        'y'], 'Both ids should have the same emotion ' \
+                              'label'
+                    prev_vec = final_data_dic[spkr][id]['x']
+                    this_vec = el_dic['x']
+
+                    assert prev_vec.shape[0] == this_vec.shape[0], \
+                        'The two arrays to be concatenated should ' \
+                        'contain the same number of segments'
+
+                    new_vec = np.concatenate([prev_vec, this_vec],
+                                             axis=1)
 
                     final_data_dic[spkr][id]['x'] = new_vec
         except Exception as e:
@@ -39,6 +46,30 @@ def fuse_all_configurations(list_of_paths):
             raise e
 
     return final_data_dic
+
+
+def safe_mkdirs(path):
+    """! Makes recursively all the directory in input path """
+    if not os.path.exists(path):
+        try:
+            os.makedirs(path)
+        except Exception as e:
+            raise IOError(
+                ("Failed to create recursive directories: "
+                " {}".format(path)
+                )
+            )
+
+
+def save_dictionary(fused_dic,
+                    save_p):
+
+    dir_path = os.path.dirname(save_p)
+    safe_mkdirs(dir_path)
+    print "Saving Fused per Segment Features Dictionary in {}".format(
+        save_p)
+    joblib.dump(fused_dic, save_p, compress=3)
+    print "OK!"
 
 
 def get_args():
@@ -49,7 +80,11 @@ def get_args():
                     'in joblib loadable format')
     parser.add_argument('-i', '--input_features_paths', nargs='+',
                         help='File paths of the features you want to '
-                             'concatenate their segment features')
+                             'concatenate their segment features',
+                        required=True)
+    parser.add_argument('-o', '--output_path', type=str,
+                        help='Path where to store the fused dictionary',
+                        required=True)
 
     args = parser.parse_args()
     return args
@@ -58,4 +93,5 @@ def get_args():
 if __name__ == "__main__":
     """!brief Example of usage"""
     args = get_args()
-    fuse_all_configurations(args.input_features_paths)
+    fused_dic = fuse_all_configurations(args.input_features_paths)
+    save_dictionary(fused_dic, args.output_path)
